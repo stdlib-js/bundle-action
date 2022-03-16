f77370aeb4038bc1a5a2d42e23729e23e9b7a33a
Philipp Burckhardt <pburckhardt@outlook.com>
2022-03-15 20:52:36 -0400

    Use ternary

--- ensure_module.js
+++ ensure_module.js
@@ -42,8 +42,8 @@ function transform( code, id ) {
 		return null;
 	}
 	const magicString = new MagicString( code );
-	magicString.prepend( 'if ( typeof module !== "undefined" ) {' );
-	magicString.append( '}' );
+	magicString.prepend( 'typeof module !== "undefined" ?' );
+	magicString.append( ': null;' );
 	return {
 		'code': magicString.toString(),
 		'map': magicString.generateMap()
